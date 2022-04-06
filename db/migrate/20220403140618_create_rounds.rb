7703fa44a0c9b09a7df3c3264ba0af58c7d50e86
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-04-06 12:36:51 +0200

    removed rounds_statistics table from database and added all of its columns into rounds

--- db/migrate/20220403140618_create_rounds.rb
+++ db/migrate/20220403140618_create_rounds.rb
@@ -1,11 +1,17 @@
 class CreateRounds < ActiveRecord::Migration[6.1]
   def change
     create_table :rounds do |t|
-      t.references :game, null: false, foreign_key: true
+      t.references :game, null: false
       t.integer :winner_points
       t.integer :loser_points
       t.string :winner
       t.string :loser
+      t.integer :oro
+      t.integer :sette_bello
+      t.integer :primera
+      t.integer :cards
+      t.integer :scopa_winner
+      t.integer :scopa_loser
 
       t.timestamps
     end
