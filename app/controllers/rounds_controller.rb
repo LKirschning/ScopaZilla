622701b04a4915a7603959c4f64193c141a39f0c
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-04-06 13:01:55 +0200

    finished rounds-controller

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,2 +1,26 @@
 class RoundsController < ApplicationController
+
+  def new
+    @round = Round.new
+  end
+
+  def create
+    @round = Round.new(round_params)
+    if @round.save
+      redirect_to game_rounds_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @rounds = Round.all
+  end
+
+  private
+
+  def round_params
+    params.require(:round).permit(:oro, :sette_bello, :primera, :cards, :scopa_loser, :scopa_winner)
+  end
+
 end
