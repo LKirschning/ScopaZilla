1c2867f1ee7671c0c9b3e1a3bba1d8ba1f439773
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-04-10 10:33:23 +0200

    implemented calculate total points in rounds_controller

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -10,12 +10,31 @@ class RoundsController < ApplicationController
     @game = Game.find(params[:game_id])
     @round = Round.new(round_params)
     @round.game = @game
+    calculate_total_points(@round)
     if @round.save
       redirect_to game_rounds_path(@game)
     else
       render :new
     end
   end
+
+  def calculate_total_points(round)
+    # this should be in the model
+    player0 = 0
+    player1 = 0
+    [round.oro, round.sette, round.primera, round.cards].each do |element|
+      if element == 0
+        player0 += 1
+      elsif element == 1
+        player1 += 1
+      end
+      player0 += round.scopa_player0 unless round.scopa_player0.nil?
+      player1 += round.scopa_player1 unless round.scopa_player1.nil?
+      round.points_player0 = player0
+      round.points_player1 = player1
+    end
+  end
+
 
   def index
     @game = Game.find(params[:game_id])
--- db/migrate/20220408080826_add_columns_to_rounds.rb
+++ db/migrate/20220408080826_add_columns_to_rounds.rb
@@ -6,7 +6,7 @@ class AddColumnsToRounds < ActiveRecord::Migration[6.1]
       add_column :rounds, column, :integer
     end
 
-    add_column :rounds, :scopa_player0, :integer
-    add_column :rounds, :scopa_player1, :integer
+    add_column :rounds, :scopa_player0, :integer, default: 0
+    add_column :rounds, :scopa_player1, :integer, default: 0
   end
 end
