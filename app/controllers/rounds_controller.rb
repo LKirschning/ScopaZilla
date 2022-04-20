3cd71b69931a569cfc211edb48deebcf5575ee9a
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-04-20 20:02:04 +0200

    fixed-new-round with calculate_total_points_function

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -10,7 +10,7 @@ class RoundsController < ApplicationController
     @game = Game.find(params[:game_id])
     @round = Round.new(round_params)
     @round.game = @game
-    calculate_total_points(@round)
+    @round = calculate_total_points(@round)
     if @round.save
       redirect_to game_rounds_path(@game)
     else
@@ -28,11 +28,13 @@ class RoundsController < ApplicationController
       elsif element == 1
         player1 += 1
       end
+    end
       player0 += round.scopa_player0 unless round.scopa_player0.nil?
       player1 += round.scopa_player1 unless round.scopa_player1.nil?
       round.points_player0 = player0
       round.points_player1 = player1
-    end
+
+      round
   end
 
 
